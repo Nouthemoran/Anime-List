845e746e4bf11a1e9c002219c82c0c9e62a2d5c1
Nouthemoran <mardiansahafiz@gmail.com>
2024-02-06 20:29:32 +0700

    page nya skrg jadi begini

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,19 @@
 import AnimeList from "@/components/AnimeList";
-import Link from "next/link";
+import Header from "@/components/AnimeList/Header";
 
-const Home = async () => {
+const Page = async () => {
 
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=10`)
-  const anime = await response.json()
+  const topAnime = await response.json()
 
   return (
-    <div>
-      <div className="flex justify-between items-center p-4">
-      <h1 className="text-2xl font-bold">PALING POPULER</h1>
-      <Link href="/populer " className="md:text-xl text-md underline hover:text-indigo-500 transition-all">Lihat Semua</Link>
-      </div>
-      <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
-      {anime.data.map(data => {
-        return (
-          <div key={data.mal_id} className="shadow-xl">
-          <AnimeList 
-          title={data.title}
-          images={data.images.webp.image_url}
-          id={data.mal_id}
-          />
-          </div>
-
-        )
-      })}
-      </div>
-    </div>
+    <>
+      <section>
+        <Header title="Paling Populer" linkTitle="Lihat Semua" linkHref="/populer"/>
+        <AnimeList api={topAnime}/>
+      </section>
+    </>
   );
 }
 
-export default Home
+export default Page
