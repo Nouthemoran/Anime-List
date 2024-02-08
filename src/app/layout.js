ffd64dcecac6fe161eb879698d3ddaf4c2ea4ba0
Nouthemoran <mardiansahafiz@gmail.com>
2024-02-08 11:44:29 +0700

    mengganti font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
-import { Inter } from "next/font/google";
+import { Gabarito } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const gabarito = Gabarito({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Man Anime List",
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${gabarito.className} bg-color-dark`}>
         <Navbar/>
         {children}
         </body>
