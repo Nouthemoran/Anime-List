05bb824388ee294c80c86c9e448ed8a7a8077d7d
Nouthemoran <mardiansahafiz@gmail.com>
2024-02-08 11:43:36 +0700

    menambahkan fungsi di search, ketika user menekan enter akan ter search

--- src/components/Navbar/InputSearch.js
+++ src/components/Navbar/InputSearch.js
@@ -7,7 +7,15 @@ import { useRef } from "react"
 const InputSearch = () => {
     const searchRef = useRef()
     const router = useRouter()
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+        handleSearch(event);
+            
+        }
+    };
+
     const handleSearch = (event) => {
+    
         event.preventDefault();
         const keyword = searchRef.current.value
         router.push(`/search/${keyword}`)
@@ -18,6 +26,7 @@ const InputSearch = () => {
                 placeholder="cari anime..." 
                 className="w-full p-2 rounded"
                 ref={searchRef}
+                onKeyDown={handleKeyDown} 
             />
             <button className="absolute top-2 end-2" onClick={handleSearch}>
             <MagnifyingGlass size={24} />
